73542f6bc7d68b19e33ac1853bb56ec9901d19c8
Maxiwell Luo <maxluoxiii@gmail.com>
2025-12-03 11:06:34 -0600

    Complete day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -11,7 +11,7 @@ fn parse_battery_bank(input: &str) -> IResult<&str, Vec<u32>> {
     many1(map(take(1usize), |d: &str| d.parse::<u32>().unwrap())).parse(input)
 }
 
-fn find_max_joltage(bank: Vec<u32>) -> u32 {
+fn find_two_digit_joltage(bank: &Vec<u32>) -> u32 {
     let mut first = 0;
     let mut second = 0;
     for (i, battery) in bank.iter().enumerate() {
@@ -26,18 +26,43 @@ fn find_max_joltage(bank: Vec<u32>) -> u32 {
     first * 10 + second
 }
 
+fn find_n_digit_joltage(bank: &Vec<u32>, n: usize) -> u64 {
+    let mut digits = vec![0; n];
+    let mut last_taken_idx = 0;
+    for (i, e) in digits.iter_mut().enumerate() {
+        let mut biggest = 0;
+        let start = if i == 0 { i } else { last_taken_idx + 1 };
+        for j in start..(bank.len() - (n - i - 1)) {
+            if bank[j] > biggest {
+                biggest = bank[j];
+                last_taken_idx = j;
+            }
+        }
+        *e = biggest;
+    }
+
+    let mut joltage = 0;
+    for (i, digit) in digits.iter().enumerate() {
+        joltage += (10u64.pow((n - i - 1) as u32)) * (*digit as u64);
+    }
+    joltage
+}
+
 fn main() {
     let args = Args::parse();
 
     let input_file = File::open(args.input).expect("Could not find file");
     let reader = BufReader::new(input_file);
 
-    let mut total_joltage = 0;
+    let mut two_digit_joltage = 0;
+    let mut twelve_digit_joltage = 0;
     for line in reader.lines() {
         let line = line.expect("Could not read line");
         let (_, bank) = parse_battery_bank(&line).expect("Could not parse line");
-        total_joltage += find_max_joltage(bank);
+        two_digit_joltage += find_two_digit_joltage(&bank);
+        twelve_digit_joltage += find_n_digit_joltage(&bank, 12);
     }
 
-    println!("Total joltage: {}", total_joltage);
+    println!("two digit joltage: {}", two_digit_joltage);
+    println!("twelve digit joltage: {}", twelve_digit_joltage);
 }
